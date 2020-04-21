e8d5d0815d7b5d542d48ed099177122b047633ae
matheusvmg <matheusvmg@gmail.com>
2020-04-21 18:55:18 -0300

    Versão atualizada dia 21/04/2020

---
+++ controle.js
@@ -0,0 +1,8 @@
+function search(){
+    var pesq = document.getElementById('pesquisar').value;
+    if(pesq === document.body){
+        alert('pesquisa encontrada!!!');
+    }else{
+        alert('pesquisa não encontrada!!!');
+    }
+}
